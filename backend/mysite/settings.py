421828a9f54d9ecb6158d72f43bfea1eda5237a4
LiocansG <nico6041@hotmail.be>
2025-04-16 16:59:47 +0200

    Mise à jour de la configuration de la base de données dans settings.py pour utiliser MySQL et ajout du fichier requirements.txt pour les dépendances nécessaires.

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    
+    "default":{
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "pidl",
+        "USER": "root",
+        "PASSWORD": "1234",
+        "HOST": "127.0.0.1",
+        "PORT": "3306"
     }
+
+
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
 }
 
 
