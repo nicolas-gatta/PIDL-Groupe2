5baee4adf8418cbde0e886011aa6cb85850304e2
LiocansG <nico6041@hotmail.be>
2025-04-20 21:29:58 +0200

    Add models for resource management and user authentication
    - Created models for Resource, Task, Optimization, Model, Evaluation, KnowledgeDistillation, ModelOptimization, ModelTask, Pruning, and Quantization in aimodel.
    - Implemented CustomUser and Role models in login with custom user manager.
    - Updated settings to use CustomUser as the AUTH_USER_MODEL.
    - Added initial migration for login app.
    - Modified SQL scripts to include foreign key constraints with ON DELETE CASCADE for better data integrity.
    - Seeded database with initial data for roles, resources, tasks, users, models, evaluations, optimizations, quantization, pruning, knowledge distillation, and model-task relationships.

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'login.CustomUser'
+
 
 # Application definition
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "login.apps.LoginConfig",
+    "aimodel.apps.AimodelConfig"
 ]
 
 MIDDLEWARE = [
@@ -116,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "UTC+2"
 
 USE_I18N = True
 
